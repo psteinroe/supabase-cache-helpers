ec9babaa2256952f85ee156e8272b8790625b5e0
psteinroe <philipp@steinroetter.com>
2022-12-17 19:15:21 +0100

    fix(postgrest-swr): mutate fn

---
+++ .changeset/angry-nails-crash.md
@@ -0,0 +1,5 @@
+---
+"@supabase-cache-helpers/postgrest-swr": patch
+---
+
+fix: mutate fn
--- packages/postgrest-swr/src/query/use-infinite-scroll-query.ts
+++ packages/postgrest-swr/src/query/use-infinite-scroll-query.ts
@@ -2,7 +2,7 @@ import { createPaginationHasMoreFetcher } from "@supabase-cache-helpers/postgres
 import { PostgrestFilterBuilder, PostgrestError } from "@supabase/postgrest-js";
 import { GenericSchema } from "@supabase/postgrest-js/dist/module/types";
 import { cloneDeep } from "lodash";
-import { useMemo } from "react";
+import { useCallback, useMemo } from "react";
 import { Middleware } from "swr";
 import useSWRInfinite, {
   SWRInfiniteConfiguration,
@@ -10,6 +10,19 @@ import useSWRInfinite, {
 } from "swr/infinite";
 
 import { createKeyGetter, infiniteMiddleware, decode } from "../lib";
+
+const parseData = (data: any[] | undefined) => {
+  if (!Array.isArray(data)) return { data, hasMore: false };
+  const newData = cloneDeep(data);
+  const lastPage = newData[newData.length - 1];
+  const lastEntry = lastPage[lastPage.length - 1];
+  return {
+    data: newData.flatMap((page: any[]) => {
+      return page.filter((i) => !i?.hasMore);
+    }),
+    hasMore: lastEntry?.hasMore ?? false,
+  };
+};
 
 type ArrayElement<ArrayType extends readonly unknown[]> =
   ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
@@ -34,47 +47,46 @@ function useInfiniteScrollQuery<
   query: PostgrestFilterBuilder<Schema, Table, Result> | null,
   config?: SWRInfiniteConfiguration & { pageSize?: number }
 ): SWRInfiniteScrollPostgrestResponse<Result[]> {
-  const { data, error, isValidating, size, setSize, isLoading, mutate } =
-    useSWRInfinite(
-      createKeyGetter(query, config?.pageSize ?? 20),
-      createPaginationHasMoreFetcher<Schema, Table, Result, [string]>(
-        query,
-        (key: string) => {
-          const decodedKey = decode(key);
-          if (!decodedKey) {
-            throw new Error("Not an SWRPostgrest key");
-          }
-          return {
-            limit: decodedKey.limit,
-            offset: decodedKey.offset,
-          };
-        },
-        config?.pageSize ?? 20
-      ),
-      {
-        ...config,
-        use: [
-          ...(config?.use ?? []),
-          infiniteMiddleware as unknown as Middleware,
-        ],
-      }
-    );
-
-  const { data: parsedData, hasMore } = useMemo(() => {
-    if (!Array.isArray(data)) return { data, hasMore: false };
-    const newData = cloneDeep(data);
-    const lastPage = newData[newData.length - 1];
-    const lastEntry = lastPage[lastPage.length - 1];
-    return {
-      data: newData.flatMap((page: any[]) => {
-        if (page[page.length - 1]?.hasMore) {
-          page.pop();
+  const {
+    data,
+    error,
+    isValidating,
+    size,
+    setSize,
+    isLoading,
+    mutate: swrMutate,
+  } = useSWRInfinite(
+    createKeyGetter(query, config?.pageSize ?? 20),
+    createPaginationHasMoreFetcher<Schema, Table, Result, [string]>(
+      query,
+      (key: string) => {
+        const decodedKey = decode(key);
+        if (!decodedKey) {
+          throw new Error("Not an SWRPostgrest key");
         }
-        return page;
-      }),
-      hasMore: lastEntry?.hasMore ?? false,
-    };
-  }, [data]);
+        return {
+          limit: decodedKey.limit,
+          offset: decodedKey.offset,
+        };
+      },
+      config?.pageSize ?? 20
+    ),
+    {
+      ...config,
+      use: [
+        ...(config?.use ?? []),
+        infiniteMiddleware as unknown as Middleware,
+      ],
+    }
+  );
+
+  const { data: parsedData, hasMore } = useMemo(() => parseData(data), [data]);
+
+  const mutate = useCallback(async () => {
+    const res = await swrMutate();
+    if (!res) return;
+    return parseData(res).data;
+  }, [swrMutate]);
 
   return {
     mutate,
