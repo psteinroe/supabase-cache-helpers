2df9bab44646298a9a5aa4be95d714577c73e9ca
psteinroe <philipp@steinroetter.com>
2023-05-09 17:51:06 +0200

    fix: parse date strings to time values

--- packages/postgrest-mutate/src/lib/if-date-get-time.ts
+++ packages/postgrest-mutate/src/lib/if-date-get-time.ts
@@ -1,2 +1,8 @@
-export const ifDateGetTime = (v: unknown) =>
-  v instanceof Date ? v.getTime() : v;
+export const ifDateGetTime = (v: unknown) => {
+  if (v instanceof Date) return v.getTime();
+  if (typeof v === 'string') {
+    const t = new Date(v).getTime();
+    if (!isNaN(t)) return t;
+  }
+  return v;
+};
