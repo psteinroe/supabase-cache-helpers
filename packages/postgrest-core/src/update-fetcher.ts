a16e84dc2a392cc3ba0fcd9385eb40c5277f8429
lauri865 <lauri.lehtmaa@gmail.com>
2024-02-13 15:55:54 +0100

    Strip primary keys from the update payload

--- packages/postgrest-core/src/update-fetcher.ts
+++ packages/postgrest-core/src/update-fetcher.ts
@@ -22,7 +22,9 @@ export type UpdateFetcherOptions<
   S extends GenericSchema,
   T extends GenericTable,
   Re = T extends { Relationships: infer R } ? R : unknown,
-> = Parameters<PostgrestQueryBuilder<S, T, Re>['update']>[1];
+> = Parameters<PostgrestQueryBuilder<S, T, Re>['update']>[1] & {
+  stripPrimaryKeys?: boolean;
+};
 
 export const buildUpdateFetcher =
   <
@@ -35,17 +37,25 @@ export const buildUpdateFetcher =
   >(
     qb: PostgrestQueryBuilder<S, T, Re>,
     primaryKeys: (keyof T['Row'])[],
-    opts: BuildNormalizedQueryOps<Q> & UpdateFetcherOptions<S, T>,
+    {
+      stripPrimaryKeys = true,
+      ...opts
+    }: BuildNormalizedQueryOps<Q> & UpdateFetcherOptions<S, T>,
   ): UpdateFetcher<T, R> =>
   async (
     input: Partial<T['Row']>,
   ): Promise<MutationFetcherResponse<R> | null> => {
-    let filterBuilder = qb.update(input as any, opts); // todo fix type;
+    const payload = { ...input };
+    let filterBuilder = qb.update(payload as any, opts); // todo fix type;
     for (const key of primaryKeys) {
       const value = input[key];
       if (!value)
         throw new Error(`Missing value for primary key ${String(key)}`);
       filterBuilder = filterBuilder.eq(key as string, value);
+
+      if (stripPrimaryKeys) {
+        payload[key] = undefined;
+      }
     }
     const query = buildNormalizedQuery<Q>(opts);
     if (query) {
