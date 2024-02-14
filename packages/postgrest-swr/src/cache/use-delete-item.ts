a4d14bdb2a286d22c624767db5fe6f21a2ee6111
psteinroe <philipp@steinroetter.com>
2024-02-14 11:04:34 +0100

    fix: make revalidate relations and table work again
    when splitting mutate and revalidate, I forgot to pass the key to
    `mutate`. this pr fixes this and adds a test.

--- packages/postgrest-swr/src/cache/use-delete-item.ts
+++ packages/postgrest-swr/src/cache/use-delete-item.ts
@@ -30,7 +30,9 @@ export function useDeleteItem<Type extends Record<string, unknown>>(
       {
         cacheKeys: getMutableKeys(Array.from(cache.keys())),
         getPostgrestFilter,
-        revalidate: (key) => mutate({ ...opts, revalidate: true }),
+        revalidate: (key) => {
+          mutate(key, { ...opts, revalidate: true });
+        },
         mutate: (key, data) => {
           mutate(key, data, { ...opts, revalidate: opts?.revalidate ?? false });
         },
