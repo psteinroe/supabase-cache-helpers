e91233c65480861431b067eb5559a289f6eb2598
psteinroe <philipp@steinroetter.com>
2024-07-12 19:22:26 +0200

    fix: run tests in single thread

--- packages/postgrest-swr/vitest.config.ts
+++ packages/postgrest-swr/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    poolOptions: { thread: { singleThread: true } },
     typecheck: { enabled: true },
     environment: 'happy-dom',
     coverage: {
