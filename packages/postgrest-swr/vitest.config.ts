4856f533fabacbf02249e34663a4ed4b6f578577
psteinroe <philipp@steinroetter.com>
2024-07-12 19:23:34 +0200

    fix: disable file concurrency for postgrest tests

--- packages/postgrest-swr/vitest.config.ts
+++ packages/postgrest-swr/vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    poolOptions: { thread: { singleThread: true } },
     typecheck: { enabled: true },
     environment: 'happy-dom',
     coverage: {
