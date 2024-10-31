a574c0e2068304b1bd20eb8a110cea1fb2291b48
Damien Schneider <74979845+damien-schneider@users.noreply.github.com>
2024-10-31 12:34:08 +0100

    ⬆️ upgrade nextra to v3.2

---
+++ docs/next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+
+import nextra from 'nextra';
+
+const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+});
+
+export default withNextra();
