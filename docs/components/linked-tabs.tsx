e36d4395d2de4c4b538150aacc05d91cfb7dbf36
Damien Schneider <74979845+damien-schneider@users.noreply.github.com>
2024-11-22 12:35:08 +0100

    :sparkles: implement linked tabs

---
+++ docs/components/linked-tabs.tsx
@@ -0,0 +1,31 @@
+import { Tabs } from 'nextra/components';
+import React, { ReactNode } from 'react';
+import { useTabContext } from './tab-context';
+
+interface LinkedTabsProps {
+  id: string;
+  items: string[];
+  children: ReactNode;
+}
+
+export const LinkedTabs: React.FC<LinkedTabsProps> = ({
+  id,
+  items,
+  children,
+}) => {
+  const { selectedTabs, setSelectedTab } = useTabContext();
+  const selectedTab = selectedTabs[id] || items[0];
+
+  const index = items.indexOf(selectedTab);
+  const selectedIndex = index !== -1 ? index : 0;
+
+  return (
+    <Tabs
+      items={items}
+      selectedIndex={selectedIndex}
+      onChange={(index: number) => setSelectedTab(id, items[index])}
+    >
+      {children}
+    </Tabs>
+  );
+};
--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,12 +1,13 @@
+import { TabProvider } from '@/components/tab-context';
 import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <TabProvider>
       <Component {...pageProps} />
       <Analytics />
-    </>
+    </TabProvider>
   );
 }
 
