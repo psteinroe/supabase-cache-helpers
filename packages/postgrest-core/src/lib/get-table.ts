e8b143898ce39762f56f27281c49892570dc4136
Zachary Hansen <zachary.r.hansen@gmail.com>
2025-01-30 14:13:56 -0800

    chore: more formatting

--- packages/postgrest-core/src/lib/get-table.ts
+++ packages/postgrest-core/src/lib/get-table.ts
@@ -1,15 +1,15 @@
 import type {
   PostgrestBuilder,
   PostgrestQueryBuilder,
-} from "@supabase/postgrest-js";
+} from '@supabase/postgrest-js';
 import {
   GenericSchema,
   GenericTable,
-} from "@supabase/postgrest-js/dist/cjs/types";
-import { getTableFromUrl } from "./get-table-from-url";
+} from '@supabase/postgrest-js/dist/cjs/types';
+import { getTableFromUrl } from './get-table-from-url';
 
 export const getTable = (
   query:
     | PostgrestBuilder<any>
     | PostgrestQueryBuilder<GenericSchema, GenericTable>
-): string => getTableFromUrl((query as { url: URL })["url"].pathname);
+): string => getTableFromUrl((query as { url: URL })['url'].pathname);
