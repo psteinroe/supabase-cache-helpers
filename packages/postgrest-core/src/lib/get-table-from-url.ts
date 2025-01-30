f20e9407ed861015881db7e6ab2c809984487cdd
Zachary Hansen <zachary.r.hansen@gmail.com>
2025-01-30 14:20:02 -0800

    chore: add comments

--- packages/postgrest-core/src/lib/get-table-from-url.ts
+++ packages/postgrest-core/src/lib/get-table-from-url.ts
@@ -7,10 +7,14 @@
  * @returns Table name
  */
 export const getTableFromUrl = (url: string): string => {
-  const split = url.toString().split('/');
-  const table = split.pop()?.split('?').shift() as string;
+  // Split the url
+  const split = url.toString().split("/");
+  // Pop the last part of the path off and remove any params if they exist
+  const table = split.pop()?.split("?").shift() as string;
+  // Pop an additional posirtion to check for rpc
   const maybeRpc = split.pop() as string;
-  return [maybeRpc === 'rpc' ? maybeRpc : null, table]
+  // Rejoin the result to include rpc otherwise just table name
+  return [maybeRpc === "rpc" ? maybeRpc : null, table]
     .filter(Boolean)
-    .join('/');
+    .join("/");
 };
