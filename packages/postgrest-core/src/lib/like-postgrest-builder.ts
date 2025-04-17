3301ae8ba9d147c164f1ead37dd9788c9c6ad9bb
Philipp Steinrötter <philipp@steinroetter.com>
2025-04-17 23:12:11 +0200

    Update like-postgrest-builder.ts

--- packages/postgrest-core/src/lib/like-postgrest-builder.ts
+++ packages/postgrest-core/src/lib/like-postgrest-builder.ts
@@ -13,16 +13,10 @@ export const isLikePostgrestBuilder = <Result>(
 ): v is LikePostgrestBuilder<Result> => {
   if (typeof v !== 'object' || v === null) return false;
   const obj = v as LikePostgrestBuilder<Result>;
-  console.log('isLikePostgrestBuilder', obj);
-
-  console.log(typeof obj['url'], typeof obj['method'], typeof obj['headers']);
-  console.log(obj['schema'], obj['body']);
-
+  
   return (
     typeof obj['url'] === 'object' &&
     typeof obj['method'] === 'string' &&
-    typeof obj['headers'] === 'object' &&
-    (obj['schema'] === undefined || typeof obj['schema'] === 'string') &&
-    (obj['body'] === undefined || typeof obj['body'] === 'object')
+    typeof obj['headers'] === 'object'
   );
 };
