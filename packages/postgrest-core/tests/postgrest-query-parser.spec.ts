c070ebf92ea7098778154068022ddce2c4cae4df
psteinroe <philipp@steinroetter.com>
2026-03-12 18:02:34 +0100

    fix: properly split at comma respecting array syntax

--- packages/postgrest-core/tests/postgrest-query-parser.spec.ts
+++ packages/postgrest-core/tests/postgrest-query-parser.spec.ts
@@ -2,6 +2,69 @@ import { PostgrestQueryParser } from '../src/postgrest-query-parser';
 import { describe, expect, it } from 'vitest';
 
 describe('PostgrestQueryParser', () => {
+  it('should parse or filters with array literals (ov operator)', () => {
+    const result = new PostgrestQueryParser(
+      'or=(gallery_external_system_types.is.null,gallery_external_system_types.ov.{euronet_v1,fee_v1})&select=id',
+    ).filters;
+    expect(result).toEqual([
+      {
+        or: [
+          {
+            alias: undefined,
+            negate: false,
+            operator: 'is',
+            path: 'gallery_external_system_types',
+            value: null,
+          },
+          {
+            alias: undefined,
+            negate: false,
+            operator: 'ov',
+            path: 'gallery_external_system_types',
+            value: '{euronet_v1,fee_v1}',
+          },
+        ],
+      },
+    ]);
+  });
+
+  it('should parse or filters with nested and containing array literals', () => {
+    const result = new PostgrestQueryParser(
+      'or=(col.is.null,and(col.ov.{a,b},col2.eq.1))&select=id',
+    ).filters;
+    expect(result).toEqual([
+      {
+        or: [
+          {
+            alias: undefined,
+            negate: false,
+            operator: 'is',
+            path: 'col',
+            value: null,
+          },
+          {
+            and: [
+              {
+                alias: undefined,
+                negate: false,
+                operator: 'ov',
+                path: 'col',
+                value: '{a,b}',
+              },
+              {
+                alias: undefined,
+                negate: false,
+                operator: 'eq',
+                path: 'col2',
+                value: 1,
+              },
+            ],
+          },
+        ],
+      },
+    ]);
+  });
+
   it('should work if the column has the name as the operator', () => {
     expect(
       new PostgrestQueryParser(
